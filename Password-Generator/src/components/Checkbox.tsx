2d34aac5680dbff1a8922ab45815747fa87f5ddb
CK-EmbPro <kennydebrice2@gmail.com>
2023-10-21 23:34:36 +0200

    React-based password generator  tool contribution

--- Password-Generator/src/components/Checkbox.tsx
+++ Password-Generator/src/components/Checkbox.tsx
@@ -1,55 +1,150 @@
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useEffect, useRef } from 'react'
 
-const Checkbox = () => {
-    const [rangeValue, setRangeValue] = useState<number>(5)
-
-    const handleRangeValueChange = (e: ChangeEvent<HTMLInputElement>)=>{
-        let parsedValue = parseInt(e.target.value, 10)
-        setRangeValue(parsedValue);
-    }
-  return (
-    <div className='w-[80%] mx-auto flex flex-col gap-6'>
-        <p>Password Length: 
-            <span>
-                {rangeValue}
-            </span>
-        </p>
-        <input type="range" className='w-full accent-[#33cccc] bg-transparent text-white ' min={5} max={30} step={1} value={rangeValue} onChange={handleRangeValueChange} />
-
-        <div className='flex flex-col gap-5'>
-
-            <div className='flex justify-between'>
-                <label className='flex gap-9' htmlFor="uppercase">
-                    Uppercase
-                </label>
-                <input type="checkbox" id='uppercase' />
-            </div>
-
-            <div className='flex justify-between'>
-                <label className='flex gap-9' htmlFor="lowercase">
-                    Lowercase
-                </label>
-                <input type="checkbox" id='lowercase' />
-            </div>
-
-            <div className='flex justify-between'>
-                <label className='flex gap-9 ' htmlFor="numbers">
-                    Numbers
-                </label>
-                <input type="checkbox" id='numbers' />
-            </div>
-
-            <div className='flex justify-between'>
-                <label className='flex gap-9 ' htmlFor="specialChars">
-                    Special Characters
-                </label>
-
-                <input type="checkbox" id='specialChars' />
-            </div>
-
-        </div>
-    </div>
-  )
+interface CheckBoxProps {
+  rangeValue: number;
+  setRangeValue: React.Dispatch<React.SetStateAction<number>>;
+  isLowercase: boolean;
+  setisLowercase: React.Dispatch<React.SetStateAction<boolean>>;
+  isUppercase: boolean;
+  setisUppercase: React.Dispatch<React.SetStateAction<boolean>>;
+  isNumbers: boolean;
+  setisNumbers: React.Dispatch<React.SetStateAction<boolean>>;
+  isSpecialChars: boolean;
+  setisSpecialChars: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default Checkbox
+const Checkbox = ({
+  rangeValue,
+  setRangeValue,
+  isLowercase,
+  setisLowercase,
+  isUppercase,
+  setisUppercase,
+  isNumbers,
+  setisNumbers,
+  isSpecialChars,
+  setisSpecialChars,
+}: CheckBoxProps) => {
+
+  const lowercaseCheckBox = useRef<HTMLInputElement>(null)
+
+  const handleRangeValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+    let parsedValue = parseInt(e.target.value, 10);
+    setRangeValue(parsedValue);
+  };
+
+  const handleLowercaseChange = () => {
+    setisLowercase(!isLowercase);
+  };
+
+  const handleUppercaseChange = () => {
+    setisUppercase(!isUppercase);
+  };
+
+  const handleNumberChange = () => {
+    setisNumbers(!isNumbers);
+  };
+
+  const handleSpecialCharsChange = () => {
+    setisSpecialChars(!isSpecialChars);
+  };
+
+  const fillStyle = {
+    width: `${((rangeValue - 5) / 25) * 100}%`,
+  };
+
+  useEffect(() => {
+    // Initialize other states as desired
+    setisUppercase(true);
+    setisNumbers(true);
+    setRangeValue(10);
+  }, []);
+
+  useEffect(() => {
+    // Check if all other states are false, and then set isLowercase to true
+    if (!isUppercase && !isNumbers && !isSpecialChars) {
+     setisLowercase(true)
+    }
+  }, [isLowercase]);
+
+  return (
+    <div className="w-[80%] mx-auto flex flex-col gap-6">
+      <p>
+        Password Length:
+        <span>{rangeValue}</span>
+      </p>
+
+      <div className="w-full">
+        <div className="relative">
+          <span className="absolute rounded-[5px]  z-[1] left-[1px] top-[7.5px] w-[100%] h-[8px] roudned-[5px] bg-[#fff]  border-[1px] border-black overflow-hidden">
+            <span className="block w-full h-[500px]  bg-[#33cccc]" style={fillStyle}></span>
+          </span>
+          <input
+            className="relative z-[2] appearance-none w-[100%] h-[10px] rounded-[5px] bg-transparent outline-none sliderThumbTailProps sliderThumbTailPropsActive"
+            type="range"
+            min="5"
+            max="30"
+            value={rangeValue}
+            onChange={handleRangeValueChange}
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-5">
+        <div className={`flex justify-between `}>
+          <label htmlFor="uppercase" className="flex justify-between w-[100%]">
+            Uppercase
+          </label>
+          <input
+            checked={isUppercase}
+            type="checkbox"
+            id="uppercase"
+            className={`appearance-none  checkBoxInput ${isUppercase ? "checkedBoxInput" : ""} `}
+            onChange={handleUppercaseChange}
+          />
+        </div>
+
+        <div className="flex justify-between">
+          <label htmlFor="lowercase" className="flex justify-between w-[100%]">
+            Lowercase
+          </label>
+          <input
+            checked={isLowercase}
+            type="checkbox"
+            id="lowercase"
+            className={`appearance-none  checkBoxInput ${isLowercase ? "checkedBoxInput" : ""} `}
+            onChange={handleLowercaseChange}
+          />
+        </div>
+
+        <div className="flex justify-between">
+          <label htmlFor="numbers" className="flex justify-between w-[100%]">
+            Numbers
+          </label>
+          <input
+            checked={isNumbers}
+            type="checkbox"
+            id="numbers"
+            className={`appearance-none  checkBoxInput ${isNumbers ? "checkedBoxInput" : ""} `}
+            onChange={handleNumberChange}
+          />
+        </div>
+
+        <div className="flex justify-between">
+          <label htmlFor="specialChars" className="flex justify-between  w-[100%]">
+            SpecialCharacters
+          </label>
+          <input
+            checked={isSpecialChars}
+            type="checkbox"
+            id="specialChars"
+            className={`appearance-none  checkBoxInput ${isSpecialChars ? "checkedBoxInput" : ""} `}
+            onChange={handleSpecialCharsChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkbox;
--- Password-Generator/src/components/PasswdGen.tsx
+++ Password-Generator/src/components/PasswdGen.tsx
@@ -1,9 +1,69 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent, useEffect, useState} from 'react'
 import PasswdGif from '../assets/gif/password.gif'
 import RefreshIcon from '../assets/icons/refresh.svg'
 import CopyIcon from '../assets/icons/copy.svg'
 
-const PasswdGen = () => {
+interface PasswdGenProps {
+  rangeValue: number;
+  setRangeValue: React.Dispatch<React.SetStateAction<number>>
+  isLowercase: boolean;
+  setisLowercase: React.Dispatch<React.SetStateAction<boolean>>;
+  isUppercase: boolean;
+  setisUppercase: React.Dispatch<React.SetStateAction<boolean>>;
+  isNumbers: boolean;
+  setisNumbers: React.Dispatch<React.SetStateAction<boolean>>;
+  isSpecialChars: boolean;
+  setisSpecialChars: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const PasswdGen = ({rangeValue, setRangeValue, isLowercase, setisLowercase, isUppercase, setisUppercase, isNumbers, setisNumbers,isSpecialChars, setisSpecialChars}: PasswdGenProps) => {
+
+  const [passwordString, setpasswordString] = useState("")
+  const [randomStringState, setrandomString] = useState("")
+  
+
+  const  generateRandomString = (passwordLen:number) : void=> {
+    const uppercase:string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const lowercase:string = "abcdefghijklmnopqrstuvwxyz"
+    const numbers:string = "0123456789"
+    const specialChars:string = "!@#$%^&*()_-+=<>?"
+    
+    let randomStringWord = ""
+      let allchars = '';
+
+      if (isUppercase) {
+        allchars += uppercase;
+      }
+      if (isLowercase) {
+        allchars += lowercase;
+      }
+      if (isNumbers) {
+        allchars += numbers;
+      }
+      if (isSpecialChars) {
+        allchars += specialChars;
+      }
+    
+      for (let i = 0; i < passwordLen; i++) {
+        let randomIndex = Math.floor(Math.random() * allchars.length);
+        randomStringWord += allchars.charAt(randomIndex);
+      }
+
+      setpasswordString(randomStringWord)
+      setrandomString(randomStringWord)
+    }
+
+    useEffect(() => {
+      generateRandomString(rangeValue);
+    },[rangeValue]);
+      
+      const handleRandomPwordChange=(e:ChangeEvent<HTMLInputElement>)=>{
+        setpasswordString(e.target.value)
+      }
+
+      console.log("uppercase", isUppercase)
+      console.log("numbers", isNumbers)
+    
   
   return (
     <div className='border-t-2xl flex flex-col items-center gap-3 pt-4'>
@@ -14,9 +74,9 @@ const PasswdGen = () => {
         <div className= 'flex flex-col w-[80%]' >
           <div className='flex  w-full justify-around items-center'>
             <div className='border border-black  relative w-[76%] px-3 py-[7px] font-bold rounded-[13px]'>
-              <input type="text" placeholder='your password' className='outline-none w-[86%]' />
-              <button className=' absolute top-[3px] h-[80%] left-[333px]'>
-                <img src={RefreshIcon} alt="" />
+              <input type="text" placeholder='your password' className='bg-transparent outline-none w-[86%]' value={passwordString} onChange={handleRandomPwordChange}  />
+              <button className=' absolute top-[3px] h-[80%] left-[350px]' onClick={()=>generateRandomString(rangeValue)}>
+                <img src={RefreshIcon}/>
               </button>
             </div>
 
@@ -26,7 +86,13 @@ const PasswdGen = () => {
             </button>
           </div>
 
-          <p className='font-bold text-[#dc3545]'>Too short</p>
+          <p className={`font-bold 
+            ${randomStringState.length<8 ? "text-[#dc3545]" :""} 
+            
+            `}>
+            {randomStringState.length <8 ? "Too short" : ""}
+            {/* {randomString} */}
+          </p>
         </div>
 
 
